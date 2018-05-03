35144ff2e6dfd68023f1acd5ae48b9db934ac7c1
Carlos Ceja Limón <cceja@ooyala.com>
2018-05-03 16:45:40 -0500

    [PLAYER-2911] Remove Ooyala Player, Using a non-smart Player

--- WebSampleApps/WebHeartbeatSSAISampleApp/ssai_heartbeat.js
+++ WebSampleApps/WebHeartbeatSSAISampleApp/ssai_heartbeat.js
@@ -1,152 +1,136 @@
-OO.plugin('heartbeat', function(OO, _, $) {
-  var log = function() {
-    OO.log.apply(this, $.merge(['heartbeat:'], arguments));
-  };
+document.addEventListener('DOMContentLoaded', function() {
 
-  var heartbeat = function(mb) {
-    var _this = this;
     var config = {};
 
     var DEFAULT_CONFIG = Object.freeze({
-      Interval: 10 * 1000,
-      ReportingPathPattern: '<hostname>/v1/vod_playback_pos/<embed_code>?ssai_guid=<ssai_guid>'
+        Interval: 10 * 1000,
+        ReportingPathPattern: '<hostname>/v1/vod_playback_pos/<embed_code>?ssai_guid=<ssai_guid>'
     });
-
-    //constants
-    _this.CONSTANTS = Object.freeze({
-      HA_ASSET_FLAG: 'ha_enabled',
-      HA_IGNORE_MAX_FLAG: 'ha_ignore_max_timeout',
-      DEFAULT_SEGMENT_LENGTH: 4,
-      PROXY_REFRESH_TIME: 6,
-    });
-
+    
+    var baseURL = '//ssai.ooyala.com/vhls/ltZ3l5YjE6lUAvBdflvcDQ-zti8q8Urd/RpOWUyOq86gFq-STNqpgzhzIcXHV/eyJvIjoiaHR0cDovL3BsYXllci5vb3lhbGEuY29tL2hscy9wbGF5ZXIvYWxsL2x0WjNsNVlqRTZsVUF2QmRmbHZjRFEtenRpOHE4VXJkLm0zdTg_dGFyZ2V0Qml0cmF0ZT0yMDAwJnNlY3VyZV9pb3NfdG9rZW49YTFkQmFraElRa3hzTDBSak9GbFBZVWd4TURWS01XUjVhMGwwWkhRNFVuUlRRM1p2VTNONVFXbDRkSEpQTW5GUFFVVkRSR3REUWtsUVIwZEpDbFkyVFdOelpHOUdaSGhtU0cxWVkzaGtORWQ1Ym1JM2VGbDNQVDBLIiwiZSI6IjE1MjQ2NzcwMTYiLCJzIjoiTThYdzhGckM5MFhEdnZlZmxWMVZxT0FQOE9TSThZRWI1dDJGSTRxWElQVT0ifQ==/manifest.m3u8?ssai_guid=HeartbeatSampleTest';
+    var embed_code = 'ltZ3l5YjE6lUAvBdflvcDQ-zti8q8Urd';
     var streamURL = '';
     var ssai_guid = '';
-    var embed_code = '';
-
+   
     var movieDuration = 0;
     var playheadPosition = 0;
     var hostname = '//ssai.ooyala.com';
     var reportingPaused = false;
 
+    var video = document.getElementById('video');
     var heartbeatTimer = 0;
 
     initialize();
 
     function initialize() {
-      config = buildConfig(config);
+        config = buildConfig(config)
 
-      //events subscribe
-      mb.subscribe(OO.EVENTS.VC_WILL_PLAY, 'heartbeat', _onVcWillPlay);
-      mb.subscribe(OO.EVENTS.EMBED_CODE_CHANGED, 'heartbeat', _onEmbedCodeChanged);
-      mb.subscribe(OO.EVENTS.PLAYHEAD_TIME_CHANGED, 'heartbeat', _onPlayheadTimeChange);
-      mb.subscribe(OO.EVENTS.PAUSE, 'heartbeat', _onPause);
-      mb.subscribe(OO.EVENTS.PLAY, 'heartbeat', _onPlay);
-      mb.subscribe(OO.EVENTS.PLAYED, 'hearbeat', _onPlayed);
-      mb.subscribe(OO.EVENTS.DESTROY, 'heartbeat', destroy)
-      
+        video.onplaying = _onVcWillPlay;
+        video.onplay = _onPlay;
+        video.onpause = _onPause;
+        video.ontimeupdate = _onPlayheadTimeChange;
+        video.onended = _onPlayed;
 
-      log('initialization completed', config);
+        // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled. 
+        // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+        // This is using the built-in support of the plain video element, without using hls.js.
+        if (video.canPlayType('application/vnd.apple.mpegurl')) {
+            video.src = baseURL;
+            video.addEventListener('canplay', function () {
+                video.play();
+            });
+        }
+        else if (Hls.isSupported()) {
+            var hls = new Hls();    
+            hls.loadSource(baseURL);
+            hls.attachMedia(video);
+            hls.on(Hls.Events.MANIFEST_PARSED, function () {
+                video.play();
+            });
+        }
+        
     }
 
-    function stopHeartBeat() {
-      clearInterval(heartbeatTimer);
+    function _onVcWillPlay() {
+        streamURL = baseURL || '';
+        ssai_guid = parseGUID(streamURL);
+        if (ssai_guid) {
+            startHeartBeat();
+        }
+    }
+
+    function _onPlay() {
+        reportingPaused = false;
+    }
+
+    function _onPlayed() {
+        reportingPaused = false;
+        reportHeartBeat()
+        stopHeartBeat()
+    }
+
+    function _onPause() {
+        reportingPaused = true;
+    }
+
+    function _onPlayheadTimeChange() {
+        movieDuration = video.duration || 0;
+        playheadPosition = video.currentTime || 0;
     }
 
     function startHeartBeat() {
-      stopHeartBeat();
+        stopHeartBeat();
 
-      heartbeatTimer = setInterval(function(){
-        if(reportingPaused){
+        heartbeatTimer = setInterval(reportHeartBeat, config.Interval);
+    }
+
+    function stopHeartBeat() {
+        clearInterval(heartbeatTimer);
+    }
+
+    function reportHeartBeat() {
+      if (reportingPaused) {
           return;
-        }
+      }
 
-        var reportingURL = config.ReportingPathPattern.replace(/<hostname>/g, hostname).replace(/<embed_code>/g, embed_code).replace(/<ssai_guid>/g, ssai_guid);
-        var data = {
+      var reportingURL = config.ReportingPathPattern.replace(/<hostname>/g, hostname).replace(/<embed_code>/g, embed_code).replace(/<ssai_guid>/g, ssai_guid);
+      var data = {
           playheadpos: parseInt(playheadPosition),
           pingfrequency: parseInt(config.Interval / 1000)
-        };
+      };
 
-        $.ajax({
+      $.ajax({
           url: reportingURL,
           type: 'POST',
           data: JSON.stringify(data),
           contentType: 'text/plain',
           dataType: 'text',
-          success:function(){
-            log('Heartbeat was sent successfully');
+          success: function () {
+              console.log('Heartbeat was sent successfully');
           }
-        });
-
-      }, config.Interval);
-    }
-
-    function parseGUID(url) {
-      var reg = new RegExp(/ssai_guid=([^&?]*)/g);
-      var result = reg.exec(url);
-
-      if ( result.length && result[1] ) {
-        return result[1];
-      }
-      return '';
-    }
-
-    function _onVcWillPlay(event, videoId, url) {
-      streamURL = url || '';
-      ssai_guid = parseGUID(streamURL);
-      if (ssai_guid) {
-        startHeartBeat();
-      }
-    }
-
-    function _onPlayheadTimeChange(event, currentTime, duration) {
-      movieDuration = duration || 0;
-      playheadPosition = currentTime || 0;
-    }
-
-    function _onEmbedCodeChanged(event, embedCode){
-      embed_code = embedCode || '';
-    }
-
-    function _onPause(){
-      reportingPaused = true;
-    }
-
-    function _onPlay(){
-      reportingPaused = false;
-    }
-
-    function _onPlayed(){
-      reportingPaused = true;
+      });
     }
 
     function buildConfig(configuration) {
-      var _config = $.extend($.extend({}, DEFAULT_CONFIG), configuration);
+        var _config = $.extend($.extend({}, DEFAULT_CONFIG), configuration);
 
-      if (!_config.segmentLength) {
-        _config.maxSegmentsToCheck = _config.maxSegmentsToCheck || DEFAULT_CONFIG.maxSegmentsToCheck;
-      } else {
-        _config.maxSegmentsToCheck = -1;
-      }
+        if (!_config.segmentLength) {
+            _config.maxSegmentsToCheck = _config.maxSegmentsToCheck || DEFAULT_CONFIG.maxSegmentsToCheck;
+        } else {
+            _config.maxSegmentsToCheck = -1;
+        }
 
-      return _config;
+        return _config;
     }
 
-    function destroy() {
-      stopHeartBeat();
+    function parseGUID(url) {
+        var reg = new RegExp(/ssai_guid=([^&?]*)/g);
+        var result = reg.exec(url);
 
-      mb.unsubscribe(OO.EVENTS.VC_WILL_PLAY, 'heartbeat');
-      mb.unsubscribe(OO.EVENTS.DESTROY, 'heartbeat');
-      mb.unsubscribe(OO.EVENTS.EMBED_CODE_CHANGED, 'heartbeat');
-      mb.unsubscribe(OO.EVENTS.PLAYHEAD_TIME_CHANGED, 'heartbeat');
-      mb.unsubscribe(OO.EVENTS.PAUSE, 'heartbeat');
-      mb.unsubscribe(OO.EVENTS.PLAY, 'heartbeat');
-      mb.unsubscribe(OO.EVENTS.PLAYED, 'hearbeat');
-
-      log('destroy');
+        if (result.length && result[1]) {
+            return result[1];
+        }
+        return '';
     }
-  };
 
-  return heartbeat;
-
-});
+}, false);
